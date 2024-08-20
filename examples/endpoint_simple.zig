b9bc518b548b969cc81679de3f90b642e780189a
unixpariah <oskar.rochowiak@tutanota.com>
2024-08-20 14:54:27 +0200

    tests and example improvement

--- examples/endpoint_simple.zig
+++ examples/endpoint_simple.zig
@@ -2,15 +2,7 @@ const std = @import("std");
 const mox = @import("mox");
 
 pub fn main() !void {
-    var dbg_gpa = if (@import("builtin").mode == .Debug) std.heap.GeneralPurposeAllocator(.{}){} else {};
-    defer if (@TypeOf(dbg_gpa) != void) {
-        _ = dbg_gpa.deinit();
-    };
-    const alloc = if (@TypeOf(dbg_gpa) != void) dbg_gpa.allocator() else std.heap.page_allocator;
-
-    var server = mox.HTTPServer.init(alloc);
-    defer server.deinit();
-
+    var server = mox.HTTPServer.init(std.heap.page_allocator);
     var port: u16 = 8080;
     while (true) {
         server.bind("127.0.0.1", port) catch {
@@ -24,7 +16,8 @@ pub fn main() !void {
     std.debug.print("Listening on: {s}:{}", .{ server.ip, server.port });
 
     var counter: i32 = 0;
-    _ = try server.setListener(.POST, "/counter/modify/{}", *i32, postCounterModify, &counter);
+    _ = try server.setListener(.POST, "/counter/increment/{}", *i32, postCounterIncrement, &counter);
+    _ = try server.setListener(.POST, "/counter/decrement/{}", *i32, postCounterDecrement, &counter);
     _ = try server.setListener(.POST, "/counter/reset", *i32, postCounterReset, &counter);
     _ = try server.setListener(.GET, "/counter", *i32, getCounter, &counter);
     _ = try server.setListener(.GET, "/exit", *mox.HTTPServer, getExit, &server);
@@ -36,7 +29,7 @@ fn getExit(_: mox.Request, _: [][]const u8, server: *mox.HTTPServer) !void {
     server.stop();
 }
 
-fn postCounterModify(request: mox.Request, parameters: [][]const u8, counter: *i32) !void {
+fn postCounterIncrement(request: mox.Request, parameters: [][]const u8, counter: *i32) !void {
     const number = try std.fmt.parseInt(i32, parameters[0], 10);
     counter.* += number;
 
@@ -45,8 +38,20 @@ fn postCounterModify(request: mox.Request, parameters: [][]const u8, counter: *i
         "Number incremented by {} by this individual: {}\n",
         .{ number, request.conn.address },
     ) }, 200);
+}
 
-    if (counter.* > 10) return error.Over10;
+fn postCounterDecrement(request: mox.Request, parameters: [][]const u8, counter: *i32) !void {
+    const number = try std.fmt.parseInt(i32, parameters[0], 10);
+    counter.* -= number;
+
+    try request.reply(
+        .{ .content_type = .TEXT, .payload = try std.fmt.allocPrint(
+            request.arena_alloc,
+            "Number decremented by {} by this individual: {}\n",
+            .{ number, request.conn.address },
+        ) },
+        200,
+    );
 }
 
 fn postCounterReset(request: mox.Request, _: [][]const u8, counter: *i32) !void {
